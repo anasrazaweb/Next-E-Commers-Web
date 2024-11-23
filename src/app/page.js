32fa2e1358671ad1281bc811659cb308e64d8390
anasrazaweb <anasrazadev@gmail.com>
2024-11-23 23:09:03 +0500

    updated code

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,15 @@
-// import Image from "next/image";
-// import Baner from "@/Components/banner/Baner";
-// import Productcard from "@/Components/AllCards";
-// import Slide from "@/Components/slide";
-// import BrowzerCards from "@/Components/BrowzerCategory";
-import Navbar from "@/Components/Navabr/Navbar";
+import "@/app/globals.css";
 import Sell from "@/Components/Timesection/Sell";
 import Product from "@/Components/bestsellproduct";
 import Hero from "@/Components/mainpage/Index";
 import Ourproduct from "@/Components/Expolore";
 import GridBox from "@/Components/Gridebanner";
 import Arival from "@/Components/Arival";
-import Page from "../app/footer/page"
 import Searchbar from "@/Components/Searchbar";
-import { AddCardContextProvider } from "@/context/ADDTOCARD/AddCardContext";
-import Deal from "@/Components/SupperDeal/Deal";
 
 export default function Home() {
   return (
     <div className=" overflow-hidden">
-      {/* <Navbar /> */}
       <div className="px-2 md:hidden block mb-5">
         <Searchbar />
       </div>
@@ -28,9 +19,6 @@ export default function Home() {
       <Ourproduct />
       <GridBox />
       <Arival />
-
-
-
     </div>
   );
 }
